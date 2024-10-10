34e99efa07ddd392ff3ca8bcc21ff8bd20f5e3af
GavinPJK <gavin.kirwan@mqube.com>
2024-10-10 12:40:35 +0100

    chore: pass SourceConfigFile path as public var

--- pkg/variablefinders/requirements.go
+++ pkg/variablefinders/requirements.go
@@ -91,9 +91,7 @@ func GetSettings(g gitclient.Interface, jxClient jxc.Interface, ns, dir, owner, 
 			return nil, "", errors.New("failed to find a dev environment source url on development environment resource")
 		}
 	}
-	// Note: attempt to sparsely clone the cluster repo to get the source-config.yaml file
-	// If sparse clone is not supported, we will fall back to a shallow clone
-	clusterDir, err := requirements.PartialCloneClusterRepo(g, gitURL, true, ".jx/gitops/source-config.yaml")
+	clusterDir, err := requirements.PartialCloneClusterRepo(g, gitURL, true, sourceconfigs.SourceConfigFile)
 	if err != nil {
 		return nil, "", err
 	}
